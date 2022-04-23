344b023e391ed17459655b2d966db4318e5d71f9
UletChuvaki <moisei.98@inbox.ru>
2022-04-23 08:59:47 +0300

    Добавьте файлы проекта.

---
+++ People.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts
+{
+    
+    
+    class People
+    {
+        protected string ffio;
+
+        public People()
+        {
+
+        }
+
+        public People(string fio, string tel)
+        {
+            ffio = fio;
+            telephone = tel;
+            
+        }
+
+        public People(string fio, string tel, string email)
+        {
+            ffio = fio;
+            telephone = tel;
+            mail = email;
+
+        }
+
+
+        public string fio {
+            get
+            {
+                return ffio;
+            }
+            set
+            {
+                ffio = value.ToUpper();
+            }
+        }
+        public string telephone;
+        public string mail;
+        public virtual void Display()
+        {
+            Console.WriteLine(fio + telephone + mail );
+            
+        }
+    }
+    class Peoples
+    {
+        public List<People> peoples;
+        public Peoples()
+        {
+            peoples = new List<People>();
+        }
+
+        public void Display()
+        {
+            foreach (var item in peoples)
+            {
+                item.Display();
+
+            }
+
+        }
+
+        public People this[int index]
+        {
+            get
+            {
+                return peoples[index];
+            }
+            set
+            {
+                peoples[index] = value;
+            }
+        }
+
+    }
+
+    class WorkerPeople : People
+    {
+        public string place { get; set; }
+        public string position { get; set; }
+        public WorkerPeople(string fio, string tel, string email, string place, string position) : base()
+        {
+            ffio = fio;
+            telephone = tel;
+            mail = email;
+            this.place = place;
+            this.position = position;
+
+        }
+
+        public WorkerPeople()
+        {
+
+        }
+        public override void Display()
+        {
+            Console.WriteLine(fio  + telephone +" mail: "+ mail +" place:" +place + " "+ position);
+        }
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Peoples peoples = new Peoples();                      
+            People people = new People();
+            people.fio = "Ivanov Ivan";
+            people.telephone = "795107869";
+            
+            peoples.peoples.Add(people);
+           
+            People people2 = new People("Ivanov Ivan 2", "895480274");
+            //people2.fio = ;
+            //people2.telephone = "795107869";
+            peoples.peoples.Add(people2);
+            
+
+
+            people = new People("Petrov Petr", "7964920", "petrov@mail.ru");
+            //people.fio = "Petrov Petr";
+            //people.telephone = "7964920";
+            peoples.peoples.Add(people);
+            //people.Display();
+
+            People wrkt = new WorkerPeople();
+            wrkt.fio = "Sergeev Sergey";
+            wrkt.telephone = "795372910";
+
+
+
+            WorkerPeople wrk = new WorkerPeople ( "Sergeev Sergey", "795372910",  "sergeev@mail.ru", "police",  "policeman"); //восходящее преобразование
+            
+            (wrk as WorkerPeople).position = "1123";
+
+            People Wrk = new WorkerPeople { 
+                             fio = "Alexandrov Alexandr", 
+                             telephone = "796840863", 
+                             mail = "alexandrovv@mail.ru", place = "restaurant", position = "cook" };
+            string Place = ((WorkerPeople)Wrk).place; //нисходящее преобразование от People к PlaceOfWork
+
+            peoples.peoples.Add(wrk);
+
+            if (peoples[1] is  WorkerPeople)
+            {
+                (peoples[1] as WorkerPeople).place = "3123";
+
+            }
+
+            peoples.Display();
+            Console.ReadKey();
+
+            
+        }
+    }
+}
